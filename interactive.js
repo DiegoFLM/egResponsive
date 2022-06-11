d78a90f473075e350ddba8352579e33bdac6a4a5
DiegoF <diego.ledesma@correounivalle.edu.co>
2022-06-11 11:38:30 -0500

    first commit

---
+++ interactive.js
@@ -0,0 +1,13 @@
+//document.getElementById("demo").innerHTML = "Hello JavaScript!";
+
+function doSomething(){
+    alert("working function");
+}
+
+function goToBeginning(){
+    document.location.href = "pagina.html";
+}
+
+function goToP1(){
+    document.location.href = "p1.html";
+}
